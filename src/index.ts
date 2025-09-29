e02b2ffa2ecc49205a5f1b5bfbb103dbf32df4d3
SukkaW <isukkaw@gmail.com>
2025-09-29 23:52:25 +0800

    refactor: prefer async iterator method

--- src/index.ts
+++ src/index.ts
@@ -70,113 +70,73 @@ export async function deleteStaleCloudflarePagesDeployments({
 
     // TODO: https://github.com/cloudflare/cloudflare-typescript/issues/2680
     // Before that is fixed, manually fire request with proper type to get all deployments
-    let deployments = await client.getAPIList(
+    for await (const deployment of await client.getAPIList(
       `/accounts/${account_id}/pages/projects/${project.name}/deployments`,
       DeploymentsMultiPage,
       { query: { page: 1 } } // this is required to get the nextPageInfo right
-    );
-
-    let totalPages;
-    if ('total_pages' in deployments.result_info && typeof deployments.result_info.total_pages === 'number') {
-      totalPages = deployments.result_info.total_pages;
-    } else {
-      throw new TypeError('Expecting "deployments.result_info.total_pages"!');
-    }
-
-    while (true) {
-      for (const deployment of deployments.result) {
-        if (!deployment.id) {
-          logger.warn('Skipping deployment without id:', deployment);
-          continue;
-        }
-        if (!deployment.created_on) {
-          logger.warn('Skipping deployment without created_on:', deployment.id);
-          continue;
-        }
-
-        const isSuccess = deployment.latest_stage?.status === 'success';
-        const isFailed = deployment.latest_stage?.status === 'failure';
-        if (isSuccess) {
-          successCount++;
-        }
-        if (isFailed) {
-          failedCount++;
-        }
-
-        const date = new Date(deployment.created_on);
-        const dateStr = formatDate(date, 'yyyy-MM-dd HH:mm:ss');
-
-        const status = deployment.is_skipped
-          ? blue('skipped')
-          : deployment.latest_stage?.status === 'success'
-            ? green('success')
-            : deployment.latest_stage?.status === 'failure'
-              ? red('failure')
-              : yellow(deployment.latest_stage?.status || 'unknown');
-
-        const urlColorFn = isFailed ? gray : underline;
-
-        const url = urlColorFn(deployment.url || 'https://unknown');
-
-        const key = `${dateStr} ${deployment.environment} ${url} (${status})`;
-        const project_name = magenta('[' + deployment.project_name + ']');
-
-        if (deployment.aliases != null && deployment.aliases.length > 0) {
-          logger.info(`${project_name} ${gray(`(skip active deployments ${deployment.aliases.join(', ')})`)} ${key}`);
-          continue;
-        }
-
-        if (startTimtstamp - date.getTime() <= retainRencentDays * 24 * 60 * 60 * 1000) {
-          logger.info(`${project_name} ${gray(`(skip recent ${retainRencentDays}d)`)} ${key}`);
-          continue;
-        }
-
-        if (isSuccess && successCount <= retainSuccessCount) {
-          logger.info(`${project_name} ${gray(`(skip first ${retainSuccessCount} succeed)`)} ${key}`);
-          continue;
-        }
-
-        if (isFailed && failedCount <= retainFailedCount) {
-          logger.info(`${project_name} ${gray(`(skip first ${retainFailedCount} failed)`)} ${key}`);
-          continue;
-        }
-
-        if (dryRun) {
-          logger.success(`${project_name} (dry run) ${key}`);
-        } else {
-          // eslint-disable-next-line no-await-in-loop -- delete in sequence
-          await client.pages.projects.deployments.delete(project.name, deployment.id, { account_id });
-          logger.success(`${project_name} (delete) ${key}`);
-        }
+    )) {
+      if (!deployment.id) {
+        logger.warn('Skipping deployment without id:', deployment);
+        continue;
+      }
+      if (!deployment.created_on) {
+        logger.warn('Skipping deployment without created_on:', deployment.id);
+        continue;
       }
 
-      if (deployments.hasNextPage()) {
-        const nextPageInfo = deployments.nextPageInfo();
+      const isSuccess = deployment.latest_stage?.status === 'success';
+      const isFailed = deployment.latest_stage?.status === 'failure';
+      if (isSuccess) {
+        successCount++;
+      }
+      if (isFailed) {
+        failedCount++;
+      }
 
-        if (
-          nextPageInfo
-          && 'params' in nextPageInfo
-          && typeof nextPageInfo.params === 'object'
-          && nextPageInfo.params
-          && 'page' in nextPageInfo.params
-          && typeof nextPageInfo.params.page === 'number'
-        ) {
-          if (nextPageInfo.params.page > totalPages) {
-            break;
-          }
-        } else {
-          throw new TypeError('Expecting "nextPageInfo.params.page"!');
-        }
+      const date = new Date(deployment.created_on);
+      const dateStr = formatDate(date, 'yyyy-MM-dd HH:mm:ss');
 
-        try {
-          // eslint-disable-next-line no-await-in-loop -- fetch in sequence
-          deployments = await deployments.getNextPage();
-        } catch {
+      const status = deployment.is_skipped
+        ? blue('skipped')
+        : deployment.latest_stage?.status === 'success'
+          ? green('success')
+          : deployment.latest_stage?.status === 'failure'
+            ? red('failure')
+            : yellow(deployment.latest_stage?.status || 'unknown');
 
-        }
+      const urlColorFn = isFailed ? gray : underline;
+
+      const url = urlColorFn(deployment.url || 'https://unknown');
+
+      const key = `${dateStr} ${deployment.environment} ${url} (${status})`;
+      const project_name = magenta('[' + deployment.project_name + ']');
+
+      if (deployment.aliases != null && deployment.aliases.length > 0) {
+        logger.info(`${project_name} ${gray(`(skip active deployments ${deployment.aliases.join(', ')})`)} ${key}`);
+        continue;
+      }
+
+      if (startTimtstamp - date.getTime() <= retainRencentDays * 24 * 60 * 60 * 1000) {
+        logger.info(`${project_name} ${gray(`(skip recent ${retainRencentDays}d)`)} ${key}`);
+        continue;
+      }
+
+      if (isSuccess && successCount <= retainSuccessCount) {
+        logger.info(`${project_name} ${gray(`(skip first ${retainSuccessCount} succeed)`)} ${key}`);
+        continue;
+      }
+
+      if (isFailed && failedCount <= retainFailedCount) {
+        logger.info(`${project_name} ${gray(`(skip first ${retainFailedCount} failed)`)} ${key}`);
+        continue;
+      }
+
+      if (dryRun) {
+        logger.success(`${project_name} (dry run) ${key}`);
       } else {
-        break;
+        await client.pages.projects.deployments.delete(project.name, deployment.id, { account_id });
+        logger.success(`${project_name} (delete) ${key}`);
       }
-    };
-  }
+    }
+  };
 }
